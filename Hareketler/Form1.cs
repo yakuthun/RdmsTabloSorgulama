8076a639ffde63256f1a69066e9f5fb088c21227
yakuthun <yakuthun@gmail.com>
2021-09-15 14:16:29 +0300

    Initial commit

---
+++ Hareketler/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Hareketler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-0NEP572;Initial Catalog=HAREKET;Integrated Security=True");
+        /*
+         * Yeni bir satış işlemini form olarak gir. 
+         * ComboBox'tan seçilerek gir. 
+         * ValueMember ve DisplayMember kullan
+         */
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("HAREKET", baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
